2dd311fc117e5400150c627a9a31dc05f3c3e079
Harvey Triana <harveytriana@hotmail.com>
2018-10-07 10:13:18 -0500

    Initial commit

---
+++ Lab/IpcTest.cs
@@ -0,0 +1,78 @@
+﻿using MML.Shared;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+// Sample in production case, external Ipc proivider
+// The model has to exist in a DLL
+// new Lab.IpcTest().RunFromExternal();
+//    
+
+namespace Lab
+{
+    class IpcTest
+    {
+        const string mapFile = "SAMP-CO-F-0001";
+
+        readonly IpcWriter w = new IpcWriter(mapFile);
+        readonly IpcReader r = new IpcReader(mapFile);
+
+        public void Run()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("C# Send Content Between Processes (Memory Mapped File)\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            Console.WriteLine("Write something...");
+
+            WriteSomething();
+
+            Console.WriteLine("Press any key to read it...");
+            Console.ReadKey();
+
+            Console.WriteLine("Read");
+
+            ReadSomething();
+
+            Console.ReadLine();
+        }
+
+        private void ReadSomething()
+        {
+            var data = r.Get<OnlineRecord>();
+
+            Console.WriteLine(data);
+        }
+
+        private void WriteSomething()
+        {
+            w.Put(Sample());
+        }
+
+        private OnlineRecord Sample()
+        {
+            var r = new OnlineRecord {
+                ID = "SAMP-CO-F-0001",
+                Time = DateTime.Now,
+                Depth = 1234.12F,
+                Content = new List<Parameter>()
+            };
+            r.Content.Add(new Parameter { ID = "ROP", Value = 122.23F });
+            r.Content.Add(new Parameter { ID = "WOB", Value = 2.423F });
+            r.Content.Add(new Parameter { ID = "RPM", Value = 120.03F });
+            return r;
+        }
+
+        // For this, imperative: The model has to be a DLL
+        // in this case I use LabCore21.Models.dll
+        public void RunFromExternal()
+        {
+            for (int i = 1; i <= 10; i++) {
+                ReadSomething();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿// Samples
+// Visonary S.A.S.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.Serialization.Formatters.Binary;
+
+// Begin: http://bit.ly/2zWrtMf
+
+namespace MmfConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Lab.IpcTest().RunFromExternal();
+            return;
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("C# Send Data Between Processes (Memory Mapped File)\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            Console.WriteLine("Write something...");
+
+            WriteSomething();
+
+            Console.WriteLine("Press any key to read it...");
+            Console.ReadLine();
+
+            Console.WriteLine("Read");
+
+            ReadSomething2();
+
+            Console.ReadLine();
+        }
+
+        private static void WriteSomething()
+        {
+            const int MMF_MAX_SIZE = 1024;  // allocated memory for this memory mapped file (bytes)
+            const int MMF_VIEW_SIZE = 1024; // how many bytes of the allocated memory can this process access
+
+            // creates the memory mapped file which allows 'Reading' and 'Writing'
+            var mmf = MemoryMappedFile.CreateOrOpen("mmf1", MMF_MAX_SIZE, MemoryMappedFileAccess.ReadWrite);
+
+            // creates a stream for this process, which allows it to write data from offset 0 to 1024 (whole memory)
+            var mmvStream = mmf.CreateViewStream(0, MMF_VIEW_SIZE);
+
+            // this is what we want to write to the memory mapped file
+            var m = new Message {
+                Title = "test",
+                Content = new List<Parameter>(),
+                Time = DateTime.Now
+            };
+            m.Content.Add(new Parameter { Mnemonic = "ROP", Value = 123.456F });
+            m.Content.Add(new Parameter { Mnemonic = "WOB", Value = 2.321F });
+            m.Content.Add(new Parameter { Mnemonic = "RPM", Value = 187.321F });
+
+            // serialize the variable 'message1' and write it to the memory mapped file
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(mmvStream, m);
+            mmvStream.Seek(0, SeekOrigin.Begin); // sets the current position back to the beginning of the stream
+        }
+
+        private static void ReadSomething()
+        {
+            const int MMF_VIEW_SIZE = 1024; // how many bytes of the allocated memory can this process access
+
+            // creates the memory mapped file
+            MemoryMappedFile mmf = MemoryMappedFile.OpenExisting("mmf1");
+            MemoryMappedViewStream mmvStream = mmf.CreateViewStream(0, MMF_VIEW_SIZE); // stream used to read data
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            // needed for deserialization
+            byte[] buffer = new byte[MMF_VIEW_SIZE];
+
+            mmvStream.Read(buffer, 0, MMF_VIEW_SIZE);
+
+            // deserializes the buffer & prints the message
+            var m = (Message)formatter.Deserialize(new MemoryStream(buffer));
+            Console.WriteLine(m);
+        }
+
+       
+        // Without use MMF_MAX_SIZE
+        private static void ReadSomething2()
+        {
+            using (var mmf = MemoryMappedFile.OpenExisting("mmf1",
+                            MemoryMappedFileRights.Read,
+                            HandleInheritability.Inheritable)) {
+
+                using (var accessor = mmf.CreateViewAccessor(0, 0, MemoryMappedFileAccess.Read)) {
+                    var n = accessor.Capacity;
+                    var buffer = new byte[n];
+                    for (int i = 0; i < n; i++) {
+                        buffer[i] = accessor.ReadByte(i);
+                    }
+                    var formatter = new BinaryFormatter();
+                    var m = (Message)formatter.Deserialize(new MemoryStream(buffer));
+
+                    Console.WriteLine(m);
+                }
+            }
+        }
+    }
+
+    [Serializable]  // mandatory
+    class Message
+    {
+        public string Title { get; set; }
+        public DateTime Time { get; set; }
+        public List<Parameter> Content { get; set; }
+
+        public override string ToString()
+        {
+            var s = $"{Time.ToString("HH:mm:ss")}: {Title}";
+            Content.ForEach(x => s += $"\n{x.Mnemonic}: {x.Value}");
+            return s;
+        }
+    }
+
+    [Serializable]  // mandatory
+    class Parameter
+    {
+        public string Mnemonic { get; set; }
+        public float Value { get; set; }
+    }
+}
